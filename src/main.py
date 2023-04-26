cdd999ea651a5ec6ae730a6a0f357f20df6f586c
Henri <hjamet@student.42.fr>
2023-04-26 18:48:02 +0200

    ci: :technologist: Add Black Formater

--- src/main.py
+++ src/main.py
@@ -5,14 +5,13 @@ from Logger import Logger
 
 
 class Mazic:
-    
     def __init__(self) -> None:
         # Instantiate Logger
-        self.logger = Logger(self.__class__.__name__,logging.INFO)
-        
+        self.logger = Logger(self.__class__.__name__, logging.INFO)
+
         # Instantiate pygame
         pygame.init()
-        
+
         # Create the display
         self.screen = pygame.display.set_mode((800, 600))
         ## Set window title
@@ -20,24 +19,24 @@ class Mazic:
         ## Set window icon
         self.icon = pygame.image.load("assets/frames/big_demon_idle_anim_f1.png")
         pygame.display.set_icon(self.icon)
-        
-        
+
         # Instantiate attributes
         self.clock = pygame.time.Clock()
         self.running = True
-        
+
     def run(self) -> None:
         # Game loop
         while self.running:
             self.clock.tick(60)
             self.events()
-            
+
     def events(self) -> None:
         # Event loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-                
+
+
 if __name__ == "__main__":
     # Run the game
-    Mazic().run() 
+    Mazic().run()
