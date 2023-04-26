099908eb6cc513e8cfae87e785463e0f574ebfbe
Henri <hjamet@student.42.fr>
2023-04-26 18:33:58 +0200

    feat: :lipstick: Add icon

--- src/main.py
+++ src/main.py
@@ -8,7 +8,12 @@ class Mazic:
         
         # Create the display
         self.screen = pygame.display.set_mode((800, 600))
+        ## Set window title
         pygame.display.set_caption("Mazic")
+        ## Set window icon
+        self.icon = pygame.image.load("assets/frames/big_demon_idle_anim_f1.png")
+        pygame.display.set_icon(self.icon)
+        
         
         # Instantiate attributes
         self.clock = pygame.time.Clock()
