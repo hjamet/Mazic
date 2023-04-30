1d2bcc6f58106e955ebbfc3c9f293ffe1b068fe8
lopilo24 <henri.jamet@epita.fr>
2023-04-30 12:25:06 +0200

    feat: :sparkles: Load animations, animatedentity and hitbox

--- src/Character.py
+++ src/Character.py
@@ -1,9 +1,32 @@
 import pygame
-from EntityManager import Entity
+from EntityManager import Entity, AnimatedEntity
 
 
-class Character(Entity):
+class Character(Entity, AnimatedEntity):
+    assets_needed = {
+        "idle": [
+            "wizzard_m_idle_anim_f0",
+            "wizzard_m_idle_anim_f1",
+            "wizzard_m_idle_anim_f2",
+            "wizzard_m_idle_anim_f3",
+        ],
+        "run": [
+            "wizzard_m_run_anim_f0",
+            "wizzard_m_run_anim_f1",
+            "wizzard_m_run_anim_f2",
+            "wizzard_m_run_anim_f3",
+        ],
+        "hit": [
+            "wizzard_m_hit_anim_f0",
+        ],
+    }
+
     def __init__(self, name: str) -> None:
-        super().__init__()
+        """A class for the players character."""
 
+        # Call parent constructors
+        Entity.__init__(self)
+        AnimatedEntity.__init__(self)
+
+        # Set attributes
         self.name = name
--- src/EntityManager.py
+++ src/EntityManager.py
@@ -19,6 +19,21 @@ class EntityManager:
     def get_free_id(self):
         return max(self.entities, key=lambda x: x.id).id + 1 if self.entities else 0
 
+    def load_asset(self, name: str) -> None:
+        """Return an asset from the assets folder.
+
+        Args:
+            name (str): The name of the asset.
+
+        Returns:
+            pygame.Surface: The asset.
+        """
+        if name in self.asset:
+            return self.asset[name]
+
+        self.asset[name] = pygame.image.load(f"assets/frames/{name}.png")
+        return self.asset[name]
+
 
 # Instantiates the EntityManager
 entity_manager = EntityManager()
@@ -34,3 +49,28 @@ class Entity:
 
         # Instantiates Logger
         self.logger = Logger(f"{self.__class__.__name__}_{self.id}")
+
+
+class AnimatedEntity(pygame.sprite.Sprite):
+    def __init__(self) -> None:
+        """A class for the visible objects in the game.
+        Manages the display and animations.
+        """
+
+        # Check if child class has assets_needed
+        if not hasattr(self, "assets_needed"):
+            raise NotImplementedError(
+                f"{self.__class__.__name__} must have assets_needed attribute."
+            )
+
+        # Load assets
+        self.animations = {}
+        for asset_type, assets in self.assets_needed.items():
+            self.animations[asset_type] = []
+            for asset in assets:
+                self.animations[asset_type].append(
+                    self.entity_manager.load_asset(asset)
+                )
+
+        # Set hitbox
+        self.hitbox = self.animations["idle"][0].get_rect()
