cf3f48612c760646117e5254d2667bc03e33f7e9
Henri Jamet <henri.jamet@epita.fr>
2023-12-18 14:44:12 +0100

    feat: :technologist: Better draw maze

--- src/MazeGenerator.py
+++ src/MazeGenerator.py
@@ -52,43 +52,50 @@ class Mazegenerator:
             pygame.image.load("assets/frames/big_demon_idle_anim_f1.png")
         )
 
+        generation_step = 0
         while True:
             window.fill((0, 0, 0))  # Clear the screen by filling it with black
 
-            # Center the maze
-            min_x = min(room.top_left[0] for room in self.rooms)
-            min_y = min(room.top_left[1] for room in self.rooms)
-            max_x = max(room.bottom_right[0] for room in self.rooms)
-            max_y = max(room.bottom_right[1] for room in self.rooms)
+            if len(self.rooms) != 0:
+                # Center the maze
+                min_x = min(room.top_left[0] for room in self.rooms)
+                min_y = min(room.top_left[1] for room in self.rooms)
+                max_x = max(room.bottom_right[0] for room in self.rooms)
+                max_y = max(room.bottom_right[1] for room in self.rooms)
 
-            # Calculate optimal window size
-            window_width = max_x - min_x
-            window_height = max_y - min_y
+                # Calculate optimal window size
+                window_width = max_x - min_x
+                window_height = max_y - min_y
 
-            # Calculate zoom
-            zoom = min(1000 / window_width, 1000 / window_height) / 1.5
+                # Calculate zoom
+                zoom = min(1000 / window_width, 1000 / window_height) / 1.5
 
-            for room in self.rooms:
-                top_left_x, top_left_y = room.top_left
-                bottom_right_x, bottom_right_y = room.bottom_right
-                width = bottom_right_x - top_left_x
-                height = bottom_right_y - top_left_y
-                pygame.draw.rect(
-                    window,
-                    (255, 255, 255),
-                    (
-                        int((top_left_x - min_x + WINDOW_SIZE[0] / 20) * zoom),
-                        int((top_left_y - min_y + WINDOW_SIZE[1] / 20) * zoom),
-                        int(width * zoom),
-                        int(height * zoom),
-                    ),
-                    width=3,
-                )
+                for room in self.rooms:
+                    top_left_x, top_left_y = room.top_left
+                    bottom_right_x, bottom_right_y = room.bottom_right
+                    width = bottom_right_x - top_left_x
+                    height = bottom_right_y - top_left_y
+                    pygame.draw.rect(
+                        window,
+                        (255, 255, 255),
+                        (
+                            int((top_left_x - min_x + WINDOW_SIZE[0] / 20) * zoom),
+                            int((top_left_y - min_y + WINDOW_SIZE[1] / 20) * zoom),
+                            int(width * zoom),
+                            int(height * zoom),
+                        ),
+                        width=3,
+                    )
 
             pygame.display.update()
 
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                # Iterate the generation step
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if generation_step == 0:
+                        print("Generating rooms...")
+                        self.a_generate_rooms(nbr_rooms=1, max_edge=100)
+                elif event.type == pygame.QUIT:
                     return
 
 
@@ -108,5 +115,4 @@ class Room:
 
 if __name__ == "__main__":
     maze = Mazegenerator()
-    maze.a_generate_rooms(nbr_rooms=30, max_edge=100)
     maze.draw_maze()
